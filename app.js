a97c767a9e86f8f74d59dbf6ba162f0b2025ea52
Sparsh Shandil <156890127+Allegiantsharksam@users.noreply.github.com>
2024-06-28 11:37:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Ensure you are using the right web3 provider (e.g., MetaMask)
 if (typeof window.ethereum !== 'undefined') {
     window.web3 = new Web3(window.ethereum);
     window.ethereum.enable();
@@ -6,65 +5,133 @@ if (typeof window.ethereum !== 'undefined') {
     console.log('No Ethereum browser extension detected.');
 }
 
-const contractAddress = 'YOUR_CONTRACT_ADDRESS';
-const contractABI = [
-    // Add your contract's ABI here
-    {
-        "constant": true,
-        "inputs": [],
-        "name": "totalSupply",
-        "outputs": [
-            {
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [
-            {
-                "name": "account",
-                "type": "address"
-            }
-        ],
-        "name": "balanceOf",
-        "outputs": [
-            {
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [
-            {
-                "name": "to",
-                "type": "address"
-            },
-            {
-                "name": "amount",
-                "type": "uint256"
-            }
-        ],
-        "name": "transfer",
-        "outputs": [
-            {
-                "name": "",
-                "type": "bool"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }
+const contractAddress = '0x9dd23a28128A1a0F2D677A3768F317Bf2f909E2B'; //My current Contract Address of Remix 
+const contractABI = [//extracted from remix ide
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
 ];
 
 const contract = new web3.eth.Contract(contractABI, contractAddress);
@@ -73,18 +140,29 @@ async function load() {
     const accounts = await web3.eth.getAccounts();
     const totalSupply = await contract.methods.totalSupply().call();
     const balance = await contract.methods.balanceOf(accounts[0]).call();
+    const lastTransactionTime = await contract.methods.getLastTransactionTime().call();
 
     document.getElementById('totalSupply').innerText = totalSupply;
     document.getElementById('balance').innerText = balance;
+    document.getElementById('lastTransactionTime').innerText = new Date(lastTransactionTime * 1000).toLocaleString(); // Convert Unix timestamp to human-readable date
 }
 
-async function transferTokens() {
-    const accounts = await web3.eth.getAccounts();
-    const recipient = document.getElementById('recipient').value;
-    const amount = document.getElementById('amount').value;
+async function deposit() {
+    const amount = document.getElementById('depositAmount').value;
 
-    await contract.methods.transfer(recipient, amount).send({ from: accounts[0] });
-    load(); // Refresh the balance after transfer
+    const amountWei = web3.utils.toWei(amount, 'ether');
+
+    await contract.methods.deposit(amountWei).send({ from: web3.eth.defaultAccount });
+
+    load(); 
+}
+
+async function withdraw() {
+    const amount = document.getElementById('withdrawAmount').value;
+
+    await contract.methods.withdraw(amount).send({ from: web3.eth.defaultAccount });
+
+    load(); 
 }
 
 window.onload = load;
